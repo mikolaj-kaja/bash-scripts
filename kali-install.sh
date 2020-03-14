8fd678c29b30f7b7c441690414fb8f6eef72a890
mikolaj-kaja <35898367+mikolaj-kaja@users.noreply.github.com>
2020-03-14 15:48:44 +0100

    Update kali-install.sh

--- kali-install.sh
+++ kali-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update -y && apt dist-upgrade -y && apt autoremove -y && apt clean && apt install -y openssh-server openvpn vim tmux kali-linux-top10
+apt update -y && apt dist-upgrade -y && apt autoremove -y && apt clean && apt install -y openssh-server openvpn vim tmux dumpcap <<< yes
